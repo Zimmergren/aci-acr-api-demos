da7c8a2536e50695debb34eef0e0e6b2b6b1dec8
Jussi Roine <jussi@roine.fi>
2019-03-05 14:50:43 +0200

    TenantIDLookupAPI Initial version

---
+++ src/TenantIDLookupAPI/Controllers/TenantIDLookupController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace TenantIDLookupAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TenantIDLookupController : ControllerBase
+    {
+        // GET api/tenantName without the suffix .onmicrosoft.com
+        [HttpGet("{tenantName}")]
+        public ActionResult<string> Get(string tenantName)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // URL for querying tenantName
+            var url = "https://login.windows.net/" + tenantName + ".onmicrosoft.com/v2.0/.well-known/openid-configuration";
+
+            string tenantID = "";
+
+            using (var response = client.GetAsync(url).Result)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    dynamic json = JsonConvert.DeserializeObject(content);
+                    tenantID = json.authorization_endpoint;
+                    tenantID = tenantID.Substring(26, 36);
+                }
+                else
+                {
+                    tenantID = "Error: " + response.StatusCode + ": " + response.ReasonPhrase;
+                }
+            }
+
+            return tenantID;
+        }
+    }
+}
